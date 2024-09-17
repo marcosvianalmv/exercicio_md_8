6bf291cfd658721256671def23ecad42084e0d64
Marcos Viana <marcosviana.lmv@gmail.com>
2024-09-16 21:14:17 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form-contatos')
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+    
+    let linha = "<tr>";
+    linha = linha + "<td>" + inputNomeContato.value + "</td>";
+    linha = linha + "<td>" + inputTelefoneContato.value + "</td>";
+    linha = linha + "<tr>";
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeContato.value = '';
+    inputTelefoneContato.value = '';
+});
